9776ed18f253bc44969417600a7d735da087c2a7
Samuel Kerosi <samuelbosire60@gmail.com>
2018-12-25 01:21:04 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ const API_KEY = 'AIzaSyCjpBuUe7SXUr2b8Lowen_7va9x5M66sE4';
 // create/declare a new component and produce some html
 
 class App extends Component{
+    render(){
     return (
     <div> 
       <SearchBar/>
    </div>
     );
+    };
 }
 // step 2: render the application
 ReactDOM.render( < App / > , document.querySelector('.container'));
